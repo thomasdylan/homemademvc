1fc68b65b405b260dfec21844d53d24956ad3578
Dylan Thomas <thomasdylan76@gmail.com>
2023-04-09 10:45:30 -0600

    Set up view functionality

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -5,10 +5,14 @@
         }
 
         public function index() {
+            $data = [
+                'title' => 'Welcome'
+            ];
             
+            $this->view('pages/index', $data);
         }
 
         public function about() {
-
+            $this->view('pages/about');
         }
     }
--- app/libraries/Controller.php
+++ app/libraries/Controller.php
@@ -13,8 +13,8 @@
 
     // Load view
     public function view($view, $data = []) {
-        if(file_exists('../app/views/' . $view . 'php')) {
-            require_once '../app/views' . $view . 'php';
+        if(file_exists('../app/views/' . $view . '.php')) {
+            require_once '../app/views/' . $view . '.php';
         } else {
             die('View does not exist');
         }
