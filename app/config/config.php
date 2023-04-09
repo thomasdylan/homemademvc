8549631b6a4053d58d9a3fd2643e50d487b628ea
Dylan Thomas <thomasdylan76@gmail.com>
2023-04-09 11:15:38 -0600

    Autoload libraries

---
+++ app/config/config.php
@@ -0,0 +1,7 @@
+<?php
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', 'http://localhost/homemademvc');
+  // Site Name
+  define('SITENAME', 'HomemadeMVC');
