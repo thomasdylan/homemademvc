f7f34df14dc22cb7afe80f8c12a49ece244452d7
Dylan Thomas <thomasdylan76@gmail.com>
2023-04-09 10:20:51 -0600

    Set controller and mapped params

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -10,11 +10,45 @@
     protected $params = [];
 
     public function __construct(){
-        $this->getUrl();
+      //print_r($this->getUrl());
+
+      $url = $this->getUrl();
+
+      // Find controllers for first value
+      if(isset($url[0]) && file_exists('../app/controllers/' . ucwords($url[0]). '.php')) {
+        // If true, set as controller
+        $this->currentController = ucwords($url[0]);
+        
+        unset($url[0]);
+      }
+
+      // Require controller
+      require_once '../app/controllers/' . $this->currentController . '.php';
+
+      // Instantiate controller
+      $this->currentController = new $this->currentController;
+
+      //Check for second part of url
+      if(isset($url[1])) {
+        if(method_exists($this->currentController, $url[1])) {
+          $this->currentMethod = $url[1];
+
+          unset($url[1]);
+        }
+      }
+
+      $this->params = $url ? array_values($url) : [];
+
+      call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
     }
 
     public function getUrl() {
-        echo $_GET['url'];
+      if(isset($_GET['url'])) {
+        $url = rtrim($_GET['url'], '/');
+        $url = filter_var($url, FILTER_SANITIZE_URL);
+        $url = explode('/', $url);
+        return $url;
+      }
     }
   } 
   
