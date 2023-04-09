4e8c89d0255bed2e518310f21e59f415fc327630
Dylan Thomas <thomasdylan76@gmail.com>
2023-04-09 15:36:06 -0600

    Database paths and header/footer

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,18 +1,21 @@
 <?php
     class Pages extends Controller {
         public function __construct() {
-            
+            $this->postModel = $this->model('Post');
         }
 
         public function index() {
             $data = [
                 'title' => 'Welcome'
             ];
-            
+
             $this->view('pages/index', $data);
         }
 
         public function about() {
-            $this->view('pages/about');
+            $data = [
+                'title' => 'About Us'
+            ];
+            $this->view('pages/about', $data);
         }
     }
