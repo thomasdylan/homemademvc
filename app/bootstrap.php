2b43c43b6f0d5fcbb2d62e0396d32965b894e6f7
Dylan Thomas <thomasdylan76@gmail.com>
2023-04-09 11:15:56 -0600

    Autoload added to bootstrap file.

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,8 +1,8 @@
 <?php
     // Load Config
     require_once 'config/config.php';
-    
-    // Load Libraries
-    require_once 'libraries/core.php';
-    require_once 'libraries/controller.php';
-    require_once 'libraries/database.php';
+
+    // Autoload Core Libraries
+    spl_autoload_register(function($className){
+        require_once 'libraries/' .$className . '.php';
+    });
