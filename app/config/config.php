9e3a68add49936ad8cbced71b66c75bfb5f0416d
Dylan Thomas <thomasdylan76@gmail.com>
2023-04-09 19:36:53 -0600

    Completed MVC Framework

---
+++ app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+  // DB Params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', '_USER_');
+  define('DB_PASS', '_PASS_');
+  define('DB_NAME', '_DB_NAME_');
+
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', '_URL_');
+  // Site Name
+  define('SITENAME', '_SITENAME_');
