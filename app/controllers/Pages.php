f69bd252a6909b7883936ce45295634ec679932a
Dylan Thomas <thomasdylan76@gmail.com>
2023-04-09 18:02:27 -0600

    Database class completed

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,13 +1,14 @@
 <?php
     class Pages extends Controller {
         public function __construct() {
-            $this->postModel = $this->model('Post');
+
         }
 
         public function index() {
             $data = [
-                'title' => 'Welcome'
+                'title' => 'Welcome',
             ];
+
 
             $this->view('pages/index', $data);
         }
--- app/libraries/Database.php
+++ app/libraries/Database.php
@@ -16,7 +16,7 @@
     private $stmt;
     private $error;
 
-    public function __construct(){
+    public function __construct() {
         // Set DSN
         $dsn = 'mysql:host=' . $this->host . ';dbname=' .$this->dbname;
         $options = array(
@@ -31,5 +31,52 @@
             $this->error = $e->getMessage();
             echo $this->error;
         }
+    }
+
+    // Statement for query
+    public function query($sql) {
+        $this->stmt = $this->dbh->prepare($sql);
+    }
+
+    // Bind values
+    public function bind($param, $value, $type = null) {
+        if(is_null($type)) {
+            switch(true) {
+                case is_int($value):
+                    $type = PDO::PARAM_INT;
+                    break;
+                case is_bool($value):
+                    $type = PDO::PARAM_BOOL;
+                    break;
+                case is_null($value):
+                    $type = PDO::PARAM_NULL;
+                    break;
+                default:
+                    $type = PDO::PARAM_STR;
+                
+            }
+        }
+
+        $this->stmt->bindValue($param, $value, $type);
+    }
+
+    // Execute statement
+    public function execute() {
+        return $this->stmt->execute();
+    }
+
+    // Get result as array of objects
+    public function resultSet() {
+        $this->execute();
+        return $this->stmt->fetchAll(PDO::FETCH_OBJ);
+    }
+
+    public function single() {
+        $this->execute();
+        return $this->stmt->fetch(PDO::FETCH_OBJ);
+    }
+
+    public function rowCount() {
+        return $this->stmt->rowCount();
     }
   }
